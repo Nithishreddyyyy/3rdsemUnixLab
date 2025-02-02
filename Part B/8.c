7c0ade33fd2c881abd591fc11476d6769af312b2
Nithish Reddy <Nithishreddyb2@gmail.com>
2025-02-02 16:40:05 +0530

    Made changes to pr_exit() of 8.c

--- Part B/8.c
+++ Part B/8.c
@@ -7,9 +7,13 @@
 void pr_exit(int status){
     if(WIFEXITED(status))
         printf("Normal termination, exit status: %d\n",WEXITSTATUS(status));
-
-    if(WIFSIGNALED(status))
+    else if(WIFSIGNALED(status))
         printf("Abnormal termination, signal number : %d\n",WTERMSIG(status));
+    else if (WIFSTOPPED(status))
+            printf("Child stopped, signal number: %d\n", WSTOPSIG(status));
+    else if(WIFCONTINUED(status)){
+        puts("Child continued\n");
+    }
 }
 
 int main(){
